c765876667b498aa7689d032b95dd497a9edc29e
Fernanda <fernanda.dsr96@gmail.com>
2023-08-16 15:55:32 -0300

    feature: Create a 2 class, Student and Competition

---
+++ src/Question_01/Student.java
@@ -0,0 +1,13 @@
+package Question_01;
+import java.util.*;
+
+public class Student {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+
+        List<Competition.Students>
+    }
+
+}
