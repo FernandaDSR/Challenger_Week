537e18fd9a17d57984fb03ba826908ab476b329f
Fernanda <fernanda.dsr96@gmail.com>
2023-08-16 16:26:47 -0300

    Feature: Coded Solved

--- src/Question_01/Competition.java
+++ src/Question_01/Competition.java
@@ -1,4 +1,27 @@
 package Question_01;
 
+import java.util.*;
+
 public class Competition {
+    public static List<Students> Students;
+
+    static class Students implements Comparable<Students> {
+
+        String name;
+        int problemSolved;
+
+        public Students(String name, int problemSolved) {
+            this.name = name;
+            this.problemSolved = problemSolved;
+        }
+
+        public int compareTo(Students otherStudent) {
+            if (this.problemSolved != otherStudent.problemSolved) {
+                return Integer.compare(otherStudent.problemSolved, this.problemSolved);
+            }
+            return this.name.compareTo(otherStudent.name);
+        }
+
+
+    }
 }
--- src/Question_01/Student.java
+++ src/Question_01/Student.java
@@ -7,7 +7,25 @@ public class Student {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
 
-        List<Competition.Students>
+        List<Competition.Students> student = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+            String[] input = scan.nextLine().split(" ");
+            String name = input[0];
+            int problemSolved = Integer.parseInt(input[1]);
+            student.add(new Competition.Students(name, problemSolved));
+        }
+
+        Collections.sort(student);
+
+        Competition.Students failedStudent = student.get(n - 1);
+
+
+        System.out.println(failedStudent.name);
+        if(failedStudent.equals(student.get(n - 1))){
+            System.out.println("You Failed");
+        }
+
     }
 
 }
