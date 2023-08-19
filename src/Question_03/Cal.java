4f0d0ffb31b7241665496537fa8b5a2ac461cfd3
Fernanda <fernanda.dsr96@gmail.com>
2023-08-19 12:35:32 -0300

    feat: Cal coded

--- src/Question_03/Cal.java
+++ src/Question_03/Cal.java
@@ -1,4 +1,26 @@
 package Question_03;
 
 public class Cal {
+
+  public static double evalueteExpression(String expression) {
+      String[] tokens = expression.split("[+\\-]");
+      String[] operators = expression.split("[0-9.]+");
+
+      double result = Double.parseDouble(tokens[0]);
+
+
+      for (int i = 1; i < tokens.length; i++) {
+
+          double operand = Double.parseDouble(tokens[i]);
+          char operator = operators[i].charAt(0);
+
+          if (operator == '+') {
+              result += operand;
+          } else if (operator == '-') {
+              result -= operand;
+          }
+      }
+      return result;
+  }
+
 }
