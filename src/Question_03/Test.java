f9f8eebe91465315a01c0783c967a7f5e89d2184
Fernanda <fernanda.dsr96@gmail.com>
2023-08-19 12:26:47 -0300

    feat: Test coded

--- src/Question_03/Test.java
+++ src/Question_03/Test.java
@@ -1,4 +1,20 @@
 package Question_03;
 
 public class Test {
+    private int n;
+    private String expression;
+
+    public Test(int n, String expression){
+        this.n = n;
+        this.expression = expression;
+    }
+
+    public int getN(){
+        return n;
+    }
+
+    public String getExpression(){
+        return expression;
+    }
+
 }
--- src/Question_03/UserCal.java
+++ src/Question_03/UserCal.java
@@ -1,4 +1,25 @@
 package Question_03;
+import java.util.*;
 
 public class UserCal {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        int testNumber = 1;
+
+        while(true){
+            String input = scan.nextLine();
+
+            if(input.equals("0")){
+                break;
+            }
+
+            int n = Integer.parseInt(input);
+            String expression = scan.nextLine();
+            Test test = new Test(n,expression);
+
+
+        }
+    }
+
 }
